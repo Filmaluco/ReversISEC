b43668247899520c50c774a96dc63a47249a18c7
Ricardo <33632491+rmcsilva@users.noreply.github.com>
2018-12-19 15:12:09 +0000

    Added: Initial IOs Project

---
+++ IsecExamManager/IsecExamManager/AddViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AddViewController.swift
+//  IsecExamManager
+//
+//  Created by Filipe Ramos Alves on 19/12/2018.
+//  Copyright © 2018 Filipe Ramos Alves. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    @IBOutlet weak var yearPicker: UIPickerView!
+    let yearData = Array(1...3)
+    let semesterData = Array(1...2)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        yearPicker.dataSource = self
+        yearPicker.delegate = self
+        yearPicker.tag = 1
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            if component == 0 {
+                return yearData.count
+            }
+                
+            else {
+                return semesterData.count
+            }
+        }
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            if component == 0 {
+                return String(yearData[row])
+            } else {
+                
+                return String(semesterData[row])
+            }
+        }
+        return ""
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IsecExamManager/IsecExamManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IsecExamManager
+//
+//  Created by Filipe Ramos Alves on 19/12/2018.
+//  Copyright © 2018 Filipe Ramos Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
