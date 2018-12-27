222cdd41d9549be981a27b09788f29e18a0a0e3f
FilipeA <filipera1995@hotmail.com>
2018-12-27 21:52:11 +0000

    Updated: Disabled vs world button

--- ReverISEC/app/src/main/java/com/example/rmcsilva/reverisectest/ReversiLogic/StateMachine/GameSetup.java
+++ ReverISEC/app/src/main/java/com/example/rmcsilva/reverisectest/ReversiLogic/StateMachine/GameSetup.java
@@ -1,7 +1,6 @@
 package com.example.rmcsilva.reverisectest.ReversiLogic.StateMachine;
 
 import com.example.rmcsilva.reverisectest.ReversiLogic.GameDataModel;
-import com.example.rmcsilva.reverisectest.ReversiLogic.GameDataModel.*;
 
 public class GameSetup extends StateAdapter {
     public GameSetup(GameDataModel g) {
